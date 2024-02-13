e1c639402cfd56459098a018491dc4266ad7cf2c
bengold1999 <bengold1999@gmail.com>
2024-02-13 21:18:23 +0200

    Add to other ball Size

--- js.js
+++ js.js
@@ -1,21 +1,26 @@
 'use strict'
 var gsize = 50
 
-function onBallClick() {
-    gsize = gsize + getRandomInt(20,60)
-   
+function onBallClick( maxDiameter) {
+    gsize = gsize + getRandomInt(20, 60)
+
 
     var ball = document.querySelector('.ball')
+    var ball2 =document.querySelector('.ball2')
     console.log(ball.style.height)
 
     ball.style.backgroundColor = getRandomColor()
 
     ball.style.width = gsize + 'px'
     ball.style.height = gsize + 'px'
+    ball2.style.width = gsize + 'px'
+    ball2.style.height = gsize + 'px'
 
-    if(gsize>=400){
-        gsize=100
+
+    if (gsize >= maxDiameter) {
+        gsize = 100
     }
+    
 
 
 }
--- utils.js
+++ utils.js
@@ -2,14 +2,13 @@ function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-  }
+}
 
-  function getRandomColor() {
+function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
-  
+}
