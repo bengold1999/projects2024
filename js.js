57ac8216d11b39aceafd1458f350dbf55658539f
bengold1999 <bengold1999@gmail.com>
2024-02-13 21:48:36 +0200

    untill mission 3

--- js.js
+++ js.js
@@ -35,8 +35,27 @@ function onBall3() {
     ball2.style.backgroundColor = 'blue'
     ball.style.backgroundColor = 'red'
 
-    
+
     // ball.style.backgroundColor = ball2.style.backgroundColor
 
 
+}
+
+
+function onBall4() {
+    gsize = 100
+    gsize += getRandomInt(20, 60)
+
+    var ball = document.querySelector('.ball')
+    var ball2 = document.querySelector('.ball2')
+
+    ball.style.width = gsize + 'px'
+    ball.style.height = gsize + 'px'
+    ball2.style.width = gsize + 'px'
+    ball2.style.height = gsize + 'px'
+}
+
+function onBall5() {
+    var body =document.querySelector('body')
+    body.style.backgroundColor = getRandomColor()
 }
