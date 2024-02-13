de5c7ae557b26819aab4983af2f7584984e6eed4
bengold1999 <bengold1999@gmail.com>
2024-02-13 19:33:26 +0200

    Add size if

--- js.js
+++ js.js
@@ -1,14 +1,26 @@
 'use strict'
-
+var gsize = 50
 
 function onBallClick() {
-
+    gsize = gsize + 50
    
+
     var ball = document.querySelector('.ball')
-    
+    console.log(ball.style.height)
+
+    ball.style.width = gsize + 'px'
+    ball.style.height = gsize + 'px'
+
+    if(gsize>=400){
+        gsize=100
+    }
 
 
-    ball.style.width += 200 + 'px'
-    ball.style.height += 200 + 'px'
-    
+}
+
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+
 }
